fd6728e4db5a8f4ae3fa49e42485b1a42946b027
Oleksandr Solianyk <salym3k@gmail.com>
2022-12-24 19:36:33 +0200

    add components Card2

---
+++ src/components/CardV2.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class CardV2 extends React.Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <h4 className="card-title">{this.props.title}</h4>
+                    <p className="card-text">{this.props.text}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+CardV2.propTypes = {
+    title: PropTypes.string,
+    text: PropTypes.string,
+}
+
+export default CardV2;

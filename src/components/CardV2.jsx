42f497673d3d458e88c9308ea966ed234e64039b
Oleksandr Solianyk <salym3k@gmail.com>
2022-12-25 19:44:34 +0200

    fix components Cardv2

--- src/components/CardV2.jsx
+++ src/components/CardV2.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class CardV2 extends React.Component {
+
     render() {
+
         return (
             <div className="card">
                 <div className="card-body">
-                    <h4 className="card-title">{this.props.title}</h4>
-                    <p className="card-text">{this.props.text}</p>
+                    {
+                        this.props.title && <h4 className="card-title">{this.props.title}</h4>
+                    }
+                    {
+                        this.props.text && <p className="card-text">{this.props.text}</p>
+                    }
                 </div>
             </div>
         )
     }
 }
-
-CardV2.defaultProps = {
-    title: null,
-    text: null,
-};
 
 CardV2.propTypes = {
     title: PropTypes.string,

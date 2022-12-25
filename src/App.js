725854c1c2384cef14d7c6f6e079603a9dcc139a
Oleksandr Solianyk <salym3k@gmail.com>
2022-12-25 22:12:14 +0200

    add components ListGroup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Card from "./components/Card";
 import CardV2 from "./components/CardV2";
 import DefinitionsList from "./components/DefinitionsList";
+// import Progress from "./components/Progress";
+import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
 
 function App() {
     const definitions = [
-        { dt: 'one', dd: 'two', id: 1 },
-        { dt: 'another term', dd: 'another description', id: 2 },
+        {dt: 'one', dd: 'two', id: 1},
+        {dt: 'another term', dd: 'another description', id: 2},
     ];
 
     return (
@@ -14,7 +17,14 @@ function App() {
             <CardV2 title={'Only TITLE'}/>
             <CardV2 text={'Only Text'}/>
             <CardV2 title={'This is TITLE'} text={'This is Text'}/>
-            <DefinitionsList data={definitions} />;
+            <DefinitionsList data={definitions}/>
+            {/*<Progress  />;*/}
+            <Alert type="primary" text="what is love?" />
+            <Alert type="secondary" text="second what is love?" />
+            <ListGroup>
+                <p>one</p>
+                <p>two</p>
+            </ListGroup>
         </div>
     );
 }

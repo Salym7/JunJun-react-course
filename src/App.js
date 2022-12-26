f600fa7d5a44c872a5a943076fab98f65b2af504
Oleksandr Solianyk <salym3k@gmail.com>
2022-12-26 18:10:58 +0200

    add components Progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Card from "./components/Card";
 import CardV2 from "./components/CardV2";
 import DefinitionsList from "./components/DefinitionsList";
-// import Progress from "./components/Progress";
+import Progress from "./components/Progress";
 import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
-// import BtnGroup from "./components/BtnGroup";
+import BtnGroup from "./components/BtnGroup";
 
 function App() {
     const definitions = [
@@ -19,14 +19,15 @@ function App() {
             <CardV2 text={'Only Text'}/>
             <CardV2 title={'This is TITLE'} text={'This is Text'}/>
             <DefinitionsList data={definitions}/>
-            {/*<Progress  />;*/}
+            <Progress percentage={40} />
+            <Progress percentage={100} />
             <Alert type="primary" text="what is love?" />
             <Alert type="secondary" text="second what is love?" />
             <ListGroup>
                 <p>one</p>
                 <p>two</p>
             </ListGroup>
-            {/*<BtnGroup />*/}
+            <BtnGroup />
         </div>
     );
 }
---
+++ src/components/Progress.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Progress extends Component {
+    render() {
+        const {percentage} = this.props
+        return (
+            <div className="progress">
+                <div className="progress">
+                    <div className="progress-bar" role="progressbar" aria-valuenow={percentage} aria-valuemin="0"
+                         aria-valuemax="100" aria-label="progressbar"  style={{width: `${percentage}px`}}>
+                    </div>
+                 </div>
+            </div>
+        );
+    }
+}
+
+Progress.propTypes = {
+    percentage: PropTypes.number,
+}
+
+export default Progress;

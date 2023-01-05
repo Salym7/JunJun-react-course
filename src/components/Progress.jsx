5a10c1cb526e06207b40dfd160553ca54bf401b5
Oleksandr Solianyk <salym3k@gmail.com>
2023-01-05 12:17:10 +0200

    fixed component Progress

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -6,11 +6,9 @@ class Progress extends Component {
         const {percentage} = this.props
         return (
             <div className="progress">
-                <div className="progress">
-                    <div className="progress-bar" role="progressbar" aria-valuenow={percentage} aria-valuemin="0"
-                         aria-valuemax="100" aria-label="progressbar"  style={{width: `${percentage}px`}}>
-                    </div>
-                 </div>
+                <div className="progress-bar" role="progressbar" aria-valuenow={percentage} aria-valuemin="0"
+                     aria-valuemax="100" aria-label="progressbar" style={{width: `${percentage}%`}}>
+                </div>
             </div>
         );
     }

da6b5a850db33199d0af58d299d0b40042b85d75
Oleksandr Solianyk <salym3k@gmail.com>
2023-01-11 18:17:50 +0200

    add component Component1

---
+++ src/components/Component1.jsx
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+
+class Component1 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            current: 0,
+            currentId: 1,
+        }
+    }
+
+    minusCurrent = () => {
+        this.setState(prevState => ({
+            current: this.state.current - 1,
+            currentId: this.state.currentId + 1,
+            data: [
+                {value: this.state.current - 1, id: this.state.currentId},
+                ...prevState.data,
+            ]
+        }))
+    }
+    plusCurrent = () => {
+        this.setState(prevState => ({
+            current: this.state.current + 1,
+            currentId: this.state.currentId + 1,
+            data: [
+                {value: this.state.current + 1, id: this.state.currentId},
+                ...prevState.data,
+            ]
+        }))
+    }
+
+    onHandle = (type) => () => {
+        if (type === "plus") return this.plusCurrent();
+        if (type === "minus") return this.minusCurrent();
+        return ''
+    }
+    deleteItem = (id) => () => {
+        this.setState(({
+            data: this.state.data.filter(item => item.id !== id)
+        }))
+    }
+
+    render() {
+        const {data} = this.state
+        const renderList = data.map((item) => (
+            <button type="button" className="list-group-item list-group-item-action"
+                    key={item.id}
+                    onClick={this.deleteItem(item.id)}>
+                {item.value}
+            </button>
+        ))
+
+        return (
+            <div>
+                <div className="mb-5">
+                    <div className="btn-group font-monospace mr-4 mt-4 " role="group">
+                        <button type="button" className="btn btn-outline-success" onClick={this.onHandle('plus')}>+
+                        </button>
+                        <button type="button" className="btn btn-outline-danger" onClick={this.onHandle('minus')}>-
+                        </button>
+                    </div>
+                    <div className="list-group component__space">
+                        {renderList}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Component1;

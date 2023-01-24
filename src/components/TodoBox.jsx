4a467f8b06d1b178fc1b33fe8561a8d1571e2cb2
Oleksandr Solianyk <salym3k@gmail.com>
2023-01-24 19:04:27 +0200

    rewrote component TodoBox to hooks

--- src/components/TodoBox.jsx
+++ src/components/TodoBox.jsx
@@ -1,75 +1,63 @@
-import React, {Component} from 'react';
+import {useState} from 'react';
 
-class TodoBox extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [{value: 'Here is empty', id: 1}, {value: 'Here too', id: 2}, {value: 'And here', id: 3}],
-            note: '',
-            currentId: 4,
-        }
+const TodoBox = () => {
+
+    const [data, setData] = useState([{value: 'Here is empty', id: 1}, {value: 'Here too', id: 2}, {
+        value: 'And here',
+        id: 3
+    }])
+    const [note, setNote] = useState('')
+    const [currentId, setCurrentId] = useState(4)
+
+    const deleteItem = (event) => {
+        const id = +event.currentTarget.getAttribute('data-id');
+        setData(data.filter(item => item.id !== id))
     }
 
-    deleteItem = (id) => () => {
-        this.setState(({
-            data: this.state.data.filter(item => item.id !== id)
-        }))
-    }
-
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        if (this.state.note.length < 3) return;
-        this.setState(prevState => ({
-            data: [
-                {value: this.state.note, id: this.state.currentId},
-                ...prevState.data,
-            ],
-            currentId: this.state.currentId + 1,
-            note: '',
-        }))
+        if (note.length < 3) return;
+        setData((data) => [{value: note, id: currentId}, ...data]);
+        setCurrentId(currentId + 1);
+        setNote('');
 
     }
 
-    onValueChange = (e) => {
-        this.setState({
-            note: e.target.value
-        })
+    const onValueChange = (e) => {
+        setNote(e.target.value)
     }
 
-
-    render() {
-
-        const renderList = this.state.data.map((item) => (
-            <div key={item.id}>
-                <div className="row m-3">
-                    <div className="col-auto">
-                        <button type="button" className="btn btn-primary btn-sm"
-                                onClick={this.deleteItem(item.id)}>Delete
-                        </button>
+    const renderList = data.map((item) => (
+        <div key={item.id}>
+            <div className="row m-3">
+                <div className="col-auto">
+                    <button type="button" className="btn btn-primary btn-sm"
+                            data-id={item.id}
+                            onClick={deleteItem}>Delete
+                    </button>
+                </div>
+                <div className="col">{item.value}</div>
+            </div>
+            <hr/>
+        </div>
+    ))
+    return (
+        <div>
+            <div className="m-3">
+                <form className="d-flex"
+                      onSubmit={onSubmit}>
+                    <div className="me-3">
+                        <input type="text" value={note} required="" className="form-control"
+                               placeholder="I am going..."
+                               onChange={onValueChange}/>
                     </div>
-                    <div className="col">{item.value}</div>
-                </div>
-                <hr/>
+                    <button type="submit" className="btn btn-primary">add</button>
+                </form>
             </div>
-        ))
-        return (
-            <div>
-                <div className="m-3">
-                    <form className="d-flex"
-                          onSubmit={this.onSubmit}>
-                        <div className="me-3">
-                            <input type="text" value={this.state.note} required="" className="form-control"
-                                   placeholder="I am going..."
-                                   onChange={this.onValueChange}/>
-                        </div>
-                        <button type="submit" className="btn btn-primary">add</button>
-                    </form>
-                </div>
-                {renderList}
-            </div>
-        );
-    }
+            {renderList}
+        </div>
+    );
 }
 
 export default TodoBox;
